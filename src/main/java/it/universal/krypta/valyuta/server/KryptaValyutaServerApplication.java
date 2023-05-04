9485799ea26fbdf720aa4b0f014d1c88ed266076
sayfullobek__ <107360172+sayfullobek@users.noreply.github.com>
2023-05-04 13:03:11 -0700

    Ishla

---
+++ src/main/java/it/universal/krypta/valyuta/server/KryptaValyutaServerApplication.java
@@ -0,0 +1,13 @@
+package it.universal.krypta.valyuta.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KryptaValyutaServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KryptaValyutaServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/it/universal/krypta/valyuta/server/KryptaValyutaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package it.universal.krypta.valyuta.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KryptaValyutaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
